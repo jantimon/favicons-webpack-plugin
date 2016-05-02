033b1205ee6183f7db7de1dd0444f35b7ac7ba81
Jan Nicklas <j.nicklas@me.com>
2016-05-02 10:20:58 +0200

    Refactor caching

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,14 +1,13 @@
 'use strict';
 var FaviconsWebpackPlugin = require('..');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
 
 module.exports = {
   context: __dirname,
   devtool: 'eval',
   entry: './src/entry.js',
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: __dirname + '/dist',
     filename: 'bundle.js'
   },
   plugins: [
