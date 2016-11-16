74c602b650f5e542fa078582ec87d7cc9e6125f9
Lukasz Bandzarewicz <lucassus@gmail.com>
2016-11-16 13:27:40 +0100

    Fix broken tests (#44)

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,13 +1,14 @@
 'use strict';
 var FaviconsWebpackPlugin = require('..');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
 
 module.exports = {
   context: __dirname,
   devtool: 'eval',
   entry: './src/entry.js',
   output: {
-    path: __dirname + '/dist',
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   plugins: [
