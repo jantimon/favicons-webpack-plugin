3f6d842e03a517bdecbb05085de02c6825538570
Bruno Dutra <brunocodutra@gmail.com>
2018-03-08 22:19:43 +0100

    test: ignore irrelevant assets for testing

--- test/util.js
+++ test/util.js
@@ -3,26 +3,56 @@ import path from 'path';
 import fs from 'fs-extra';
 import webpack from 'webpack';
 import merge from 'webpack-merge';
-import denodeify from 'denodeify';
 import dircompare from 'dir-compare';
+
+import {tapAsync} from '../lib/util';
 
 const fixtures = path.resolve(__dirname, 'fixtures');
 module.exports.expected = path.resolve(fixtures, 'expected');
 module.exports.logo = path.resolve(fixtures, 'logo.svg');
 
-module.exports.generate = async (config) => await denodeify(webpack)(merge({
-  entry: path.resolve(fixtures, 'entry.js'),
-  output: {
-    filename: 'bundle.js',
-    path: await fs.mkdtemp(path.join(os.tmpdir(), 'WWP')),
-  },
-}, config));
+module.exports.generate = async (config) => {
+  config = merge(
+    {
+      entry: path.resolve(fixtures, 'entry.js'),
+      output: {
+        path: await fs.mkdtemp(path.join(os.tmpdir(), 'WWP')),
+      },
+    },
+    config,
+  );
+
+  config.plugins
+    .filter(plugin => plugin.constructor.name === 'HtmlWebpackPlugin')
+    .forEach(plugin => {
+      plugin.options.chunks = [];
+    });
+
+  const compiler = webpack(config);
+
+  tapAsync(compiler, 'emit', 'Test', ({assets}, callback) => {
+    Object.keys(assets)
+      .filter(asset => asset.match(/.js$/))
+      .forEach(asset => {
+        delete assets[asset];
+      });
+
+    callback();
+  });
+
+  return await new Promise((resolve, reject) => {
+    compiler.run((err, stats) => (err || stats.hasErrors())
+      ? reject(err || stats.toJson().errors)
+      : resolve(stats)
+    );
+  });
+};
 
 module.exports.compare = async (a, b) => {
   const diff = await dircompare.compare(a, b, {
     compareContent: true,
-    excludeFilter: 'bundle.js',
   });
+
   return diff.diffSet.filter(({state}) => state !== 'equal').map(({name1, name2}) => (
     `${path.join(a, name1 + '')} ≠ ${path.join(b, name2 + '')}`)
   );
