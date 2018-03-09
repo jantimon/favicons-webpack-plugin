d64ba93f20bf12c617429bca4c9a626d02b7efdd
Bruno Dutra <brunocodutra@gmail.com>
2018-03-09 23:57:38 +0100

    remove the arguably useless example

--- example/build.js
+++
@@ -1,8 +0,0 @@
-require('webpack')(require('./webpack.config.js'), function(error, stats) {
-  if (error || stats.hasErrors()) {
-    console.error(error || stats.toJson().errors);
-    process.exit(-1);
-  }
-
-  console.log(stats.toString());
-});
