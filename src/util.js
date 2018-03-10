dc9bb74b45df7a164ab2555117f8b2ccf1085550
Bruno Dutra <brunocodutra@gmail.com>
2018-03-10 17:45:28 +0100

    setup test coverage report

--- src/util.js
+++ src/util.js
@@ -3,18 +3,21 @@ const fs = require('fs');
 const parseAuthor = require('parse-author');
 const camelCase = require('camelcase');
 
+/* istanbul ignore next */
 module.exports.getPublicPath = ({outputOptions: {publicPath = ''}}) => (
     (publicPath.length && publicPath.substr(-1) !== '/')
-  ? publicPath += '/'
+  ? publicPath + '/'
   : publicPath
 );
 
+/* istanbul ignore next */
 module.exports.getAssetPath = ({mainTemplate}, name, args) => (
     mainTemplate.getAssetPath /* Webpack >= 4.0 */
   ? mainTemplate.getAssetPath(name, args)
   : mainTemplate.applyPluginsWaterfall('asset-path', name, args)
 );
 
+/* istanbul ignore next */
 module.exports.tap = (tappable, hook, name, plugin) => (
     tappable.hooks /* Webpack >= 4.0 */
   ? tappable.hooks[camelCase(hook)] && tappable.hooks[camelCase(hook)].tapAsync(name, plugin)
