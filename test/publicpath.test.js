3e3f22830d277351ff07c3b3f1521f41e742f69a
Bruno Dutra <brunocodutra@gmail.com>
2018-03-30 19:59:12 +0200

    test: add tests for custom publicPath

---
+++ test/publicpath.test.js
@@ -0,0 +1,24 @@
+const test = require('ava');
+const path = require('path');
+const fs = require('fs-extra');
+const FaviconsWebpackPlugin = require('../');
+
+const {logo, generate, mkdir, compare, expected} = require('./util');
+
+test.beforeEach(async t => t.context.root = await mkdir());
+
+test('should take the public path into account', async t => {
+  const dist = path.join(t.context.root, 'dist');
+  await generate({
+    context: t.context.root,
+    output: {
+      path: dist,
+      publicPath: '/public/path',
+    },
+    plugins: [new FaviconsWebpackPlugin({logo})],
+  });
+
+  t.deepEqual(await compare(dist, path.resolve(expected, 'publicpath')), []);
+});
+
+test.afterEach(t => fs.remove(t.context.root));
