337293d02d65e9ddc199145de533dfa111bdeb0a
Roman O <ceo.roman@gmail.com>
2018-04-01 11:03:01 +0300

    Cleanup, update dependencies, refactoring

--- index.js
+++ index.js
@@ -3,158 +3,180 @@ const assert = require('assert')
 const fs = require('fs')
 const path = require('path')
 
-function AppManifestWebpackPlugin(options) {
-  if (typeof options === 'string') {
-    options = { logo: options }
-  }
-  assert(typeof options === 'object', 'AppManifestWebpackPlugin options are required')
-  assert(options.logo, 'An input file is required')
-  this.options = {
-    ...{
-      emitStats: false,
-      statsFilename: 'iconstats-[md5:hash:8].json',
-      persistentCache: true,
-      inject: true,
+const DEFAULT_OPTIONS = {
+  emitStats: false,
+  statsFilename: 'iconstats-[hash].json',
+  persistentCache: true,
+  inject: true,
+  config: {
+    appName: 'Webpack App',
+    appDescription: null,
+    developerName: null,
+    developerURL: null,
+    background: '#fff',
+    theme_color: '#fff',
+    path: '/',
+    display: 'standalone',
+    orientation: 'portrait',
+    start_url: '/?homescreen=1',
+    version: '1.0',
+    logging: false,
+    icons: {
+      android: true,
+      appleIcon: true,
+      appleStartup: true,
+      coast: { offset: 25 },
+      favicons: true,
+      firefox: true,
+      windows: true,
+      yandex: true,
     },
-    ...options,
-  }
-
-  this.options.config = {
-    ...{
-      appName: 'Webpack App', // Your application's name. `string`
-      appDescription: null, // Your application's description. `string`
-      developerName: null, // Your (or your developer's) name. `string`
-      developerURL: null, // Your (or your developer's) URL. `string`
-      background: '#fff', // Background colour for flattened icons. `string`
-      theme_color: '#fff', // Theme color for browser chrome. `string`
-      path: '/', // Path for overriding default icons path. `string`
-      display: 'standalone', // Android display: "browser" or "standalone". `string`
-      orientation: 'portrait', // Android orientation: "portrait" or "landscape". `string`
-      start_url: '/?homescreen=1', // Android start application's URL. `string`
-      version: '1.0', // Your application's version number. `number`
-      logging: false, // Print logs to console? `boolean`
-      icons: {
-        // Platform Options:
-        // - offset - offset in percentage
-        // - shadow - drop shadow for Android icons, available online only
-        // - background:
-        //   * false - use default
-        //   * true - force use default, e.g. set background for Android icons
-        //   * color - set background for the specified icons
-        //
-        android: true, // Create Android homescreen icon. `boolean` or `{ offset, background, shadow }`
-        appleIcon: true, // Create Apple touch icons. `boolean` or `{ offset, background }`
-        appleStartup: true, // Create Apple startup images. `boolean` or `{ offset, background }`
-        coast: { offset: 25 }, // Create Opera Coast icon with offset 25%. `boolean` or `{ offset, background }`
-        favicons: true, // Create regular favicons. `boolean`
-        firefox: true, // Create Firefox OS icons. `boolean` or `{ offset, background }`
-        windows: true, // Create Windows 8 tile icons. `boolean` or `{ background }`
-        yandex: true, // Create Yandex browser icon. `boolean` or `{ background }`
-      },
-    },
-    ...this.options.config,
-  }
+  },
 }
 
-AppManifestWebpackPlugin.prototype.apply = function(compiler) {
-  if (!this.options.config.appName) {
-    this.options.config.appName = guessAppName(compiler.context)
-  }
+const PLUGIN_NAME = 'AppManifestWebpackPlugin'
 
-  // Generate the favicons
-  let compilationResult
+const EVENTS = {
+  MAKE: `${PLUGIN_NAME}Make`,
+  COMPILE: `${PLUGIN_NAME}Compile`,
+  EMIT: `${PLUGIN_NAME}Emit`,
+  HTML_BEFORE: `${PLUGIN_NAME}HtmlPluginBefore`,
+}
+
+class AppManifestWebpackPlugin {
+  constructor(options) {
+    if (typeof options === 'string') {
+      options = { logo: options }
+    }
+
+    assert(typeof options === 'object', 'app-manifest-webpack-plugin: options are required')
+    assert(options.logo, 'An input file of icon is required')
+
+    this.options = {
+      ...DEFAULT_OPTIONS,
+      ...options,
+    }
+    this.compilationResult = null
+  }
 
   /**
-   * Make handler
-   * @param {object} compilation
-   * @param {function} callback
+   * Tries to guess the name from the package.json
+   *
+   * @param {string} pwd Path of work dir
    */
-  const makeHandler = (compilation, callback) => {
-    childCompiler
-      .compileTemplate(this.options, compiler.context, compilation)
-      .then(result => {
-        compilationResult = result
-        callback()
-      })
-      .catch(callback)
-  }
+  guessAppName(pwd) {
+    let packageJson = path.resolve(pwd, 'package.json')
 
-  if (compiler.hooks) {
-    compiler.hooks.make.tapAsync('AppManifestWebpackPluginMake', makeHandler)
-  } else {
-    compiler.plugin('make', makeHandler)
-  }
-
-  // Hook into the html-webpack-plugin processing
-  // and add the html
-  if (this.options.inject) {
-    const addFaviconsToHtml = (htmlPluginData, callback) => {
-      if (htmlPluginData.plugin.options.favicons !== false) {
-        htmlPluginData.html = htmlPluginData.html.replace(
-          /(<\/head>)/i,
-          compilationResult.stats.html.join('\n') + '$&',
-        )
+    if (!fs.existsSync(packageJson)) {
+      packageJson = path.resolve(pwd, '../package.json')
+      if (!fs.existsSync(packageJson)) {
+        return 'Webpack App'
       }
-      callback(null, htmlPluginData)
     }
-
-    // webpack 4
-    if (compiler.hooks) {
-      let tapped = 0
-
-      compiler.hooks.compilation.tap('AppManifestWebpackPlugin', cmpp => {
-        compiler.hooks.compilation.tap('HtmlWebpackPluginHooks', function() {
-          if (!tapped++) {
-            cmpp.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync(
-              'app-manifest-webpack-plugin',
-              addFaviconsToHtml,
-            )
-          }
-        })
-      })
-    } else {
-      compiler.plugin('compilation', compilation => {
-        compilation.plugin('html-webpack-plugin-before-html-processing', addFaviconsToHtml)
-      })
-    }
+    return JSON.parse(fs.readFileSync(packageJson)).name
   }
 
   /**
    * Emit handler
    *
-   * @param {object} compilation
-   * @param {function} callback
+   * @param {object} compiler
    */
-  const emitHandler = (compilation, callback) => {
-    delete compilation.assets[compilationResult.outputName]
-    callback()
-  }
-
-  // Remove the stats from the output if they are not required
-  if (!this.options.emitStats) {
-    if (compiler.hooks) {
-      compiler.hooks.emit.tapAsync('AppManifestWebpackPluginEmit', emitHandler)
+  emitHandler(compiler) {
+    if (this.beforeV4) {
+      compiler.plugin('emit', (compilation, cb) => this.emitProccess(compilation, cb))
     } else {
-      compiler.plugin('emit', emitHandler)
+      compiler.hooks['emit'].tap(EVENTS.EMIT, (compilation, cb) =>
+        this.emitProccess(compilation, cb),
+      )
     }
   }
-}
 
-/**
- * Tries to guess the name from the package.json
- *
- * @param {string} compilerWorkingDirectory Path of work dir
- */
-function guessAppName(compilerWorkingDirectory) {
-  let packageJson = path.resolve(compilerWorkingDirectory, 'package.json')
+  emitProccess(compilation, cb) {
+    delete compilation.assets[this.compilationResult.outputName]
 
-  if (!fs.existsSync(packageJson)) {
-    packageJson = path.resolve(compilerWorkingDirectory, '../package.json')
-    if (!fs.existsSync(packageJson)) {
-      return 'Webpack App'
+    if (cb) cb(null)
+  }
+
+  htmlPluginHandler(compilation) {
+    if (this.beforeV4) {
+      compilation.plugin('html-webpack-plugin-before-html-processing', (htmlPluginData, cb) => {
+        this.beforeHtmlProccessingFn(htmlPluginData, cb)
+      })
+    } else {
+      if (compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing) {
+        compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync(
+          EVENTS.HTML_BEFORE,
+          this.beforeHtmlProccessingFn,
+        )
+      }
     }
   }
-  return JSON.parse(fs.readFileSync(packageJson)).name
+
+  beforeHtmlProccessingFn(htmlPluginData, cb) {
+    // Hook into the html-webpack-plugin processing
+    // and add the html
+    if (htmlPluginData.plugin.options.favicons !== false) {
+      htmlPluginData.html = htmlPluginData.html.replace(
+        /(<\/head>)/i,
+        this.compilationResult.stats.html.join('\n') + '$&',
+      )
+    }
+    cb(null, htmlPluginData)
+  }
+
+  /**
+   * Compile handler
+   * @param {object} compiler
+   * @param {object} compilation
+   * @param {function} cb
+   */
+  compileHandler(compiler, compilation, cb) {
+    childCompiler
+      .compileTemplate(this.options, compiler.context, compilation)
+      .then(result => {
+        this.compilationResult = result
+        cb(null)
+      })
+      .catch(cb)
+  }
+
+  /**
+   * Apply plugin
+   *
+   * @param {object} compiler
+   */
+  apply(compiler) {
+    this.beforeV4 = !compiler.hooks
+
+    if (!this.options.config.appName) {
+      this.options.config.appName = this.guessAppName(compiler.context)
+    }
+
+    const compile = this.compileHandler.bind(this, compiler)
+
+    if (this.beforeV4) {
+      compiler.plugin('make', compile)
+    } else {
+      compiler.hooks['make'].tapAsync(EVENTS.MAKE, compile)
+    }
+
+    if (this.options.inject) {
+      if (this.beforeV4) {
+        compiler.plugin('compilation', this.htmlPluginHandler.bind(this))
+      } else {
+        compiler.hooks['compilation'].tap(EVENTS.COMPILE, compilation => {
+          compiler.hooks.compilation.tap(
+            'HtmlWebpackPluginHooks',
+            this.htmlPluginHandler.bind(this),
+          )
+        })
+      }
+    }
+
+    if (!this.options.emitStats) {
+      this.emitHandler(compiler)
+    }
+  }
 }
 
 module.exports = AppManifestWebpackPlugin
