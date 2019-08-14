brunocodutra/webapp-webpack-plugin

remove stats output

lib/compiler.js
@@ -6,14 +6,13 @@ module.exports.compileTemplate = function compileTemplate (options, context, com
   // The entry file is just an empty helper as the dynamic template
   // require is added in "loader.js"
   var outputOptions = {
-    filename: options.statsFilename,
+    filename: '[hash]',
     publicPath: compilation.outputOptions.publicPath
   };
   // Create an additional child compiler which takes the template
   // and turns it into an Node.JS html factory.
   // This allows us to use loaders during the compilation
-  var compilerName = getCompilerName(context, outputOptions.filename);
-  var childCompiler = compilation.createChildCompiler(compilerName, outputOptions);
+  var childCompiler = compilation.createChildCompiler('webapp-webpack-plugin', outputOptions);
   childCompiler.context = context;
   childCompiler.apply(
     new SingleEntryPlugin(context, '!!' + require.resolve('./favicons.js') + '?' +
@@ -54,14 +53,6 @@ module.exports.compileTemplate = function compileTemplate (options, context, com
   // Compile and return a promise
   return new Promise(function (resolve, reject) {
     childCompiler.runAsChild(function (err, entries, childCompilation) {
-      if (err) {
-        return reject(err);
-      }
-      // Replace [hash] placeholders in filename
-      var outputName = compilation.mainTemplate.applyPluginsWaterfall('asset-path', outputOptions.filename, {
-        hash: childCompilation.hash,
-        chunk: entries[0]
-      });
       // Resolve / reject the promise
       if (childCompilation && childCompilation.errors && childCompilation.errors.length) {
         var errorDetails = childCompilation.errors.map(function (error) {
@@ -71,20 +62,16 @@ module.exports.compileTemplate = function compileTemplate (options, context, com
       } else if (err) {
         reject(err);
       } else {
-        resolve({
-          outputName: outputName,
-          stats: JSON.parse(childCompilation.assets[outputName].source())
+        // Replace [hash] placeholders in filename
+        var outputName = compilation.mainTemplate.applyPluginsWaterfall('asset-path', outputOptions.filename, {
+          hash: childCompilation.hash,
+          chunk: entries[0]
         });
+        var stats = JSON.parse(childCompilation.assets[outputName].source());
+        delete compilation.assets[outputName];
+        childCompilation.assets = {};
+        resolve(stats);
       }
     });
   });
 };
-
-/**
- * Returns the child compiler name e.g. 'html-webpack-plugin for "index.html"'
- */
-function getCompilerName (context, filename) {
-  var absolutePath = path.resolve(context, filename);
-  var relativePath = path.relative(context, absolutePath);
-  return 'webapp-webpack-plugin for "' + (absolutePath.length < relativePath.length ? absolutePath : relativePath) + '"';
-}

jantimon/favicons-webpack-plugin

lib/compiler.js
@@ -6,14 +6,13 @@ module.exports.compileTemplate = function compileTemplate (options, context, com
   // The entry file is just an empty helper as the dynamic template
   // require is added in "loader.js"
   var outputOptions = {
-    filename: options.statsFilename,
+    filename: '[hash]',
     publicPath: compilation.outputOptions.publicPath
   };
   // Create an additional child compiler which takes the template
   // and turns it into an Node.JS html factory.
   // This allows us to use loaders during the compilation
-  var compilerName = getCompilerName(context, outputOptions.filename);
-  var childCompiler = compilation.createChildCompiler(compilerName, outputOptions);
+  var childCompiler = compilation.createChildCompiler('favicon-webpack-plugin', outputOptions);
   childCompiler.context = context;
   childCompiler.apply(
     new SingleEntryPlugin(context, '!!' + require.resolve('./favicons.js') + '?' +
@@ -54,23 +53,6 @@ module.exports.compileTemplate = function compileTemplate (options, context, com
   // Compile and return a promise
   return new Promise(function (resolve, reject) {
     childCompiler.runAsChild(function (err, entries, childCompilation) {
-      if (err) {
-        return reject(err);
-      }
-      // Replace [hash] placeholders in filename
-      // In webpack 4 the plugin interface changed, so check for available fns
-      var outputName = compilation.mainTemplate.getAssetPath
-        ? compilation.mainTemplate.hooks.assetPath.call(outputOptions.filename, {
-          hash: childCompilation.hash,
-          chunk: entries[0]
-        })
-        : compilation.mainTemplate.applyPluginsWaterfall(
-            'asset-path',
-            outputOptions.filename,
-          {
-            hash: childCompilation.hash,
-            chunk: entries[0]
-          });
       // Resolve / reject the promise
       if (childCompilation && childCompilation.errors && childCompilation.errors.length) {
         var errorDetails = childCompilation.errors.map(function (error) {
@@ -80,10 +62,15 @@ module.exports.compileTemplate = function compileTemplate (options, context, com
       } else if (err) {
         reject(err);
       } else {
-        resolve({
-          outputName: outputName,
-          stats: JSON.parse(childCompilation.assets[outputName].source())
+        // Replace [hash] placeholders in filename
+        var outputName = compilation.mainTemplate.applyPluginsWaterfall('asset-path', outputOptions.filename, {
+          hash: childCompilation.hash,
+          chunk: entries[0]
         });
+        var stats = JSON.parse(childCompilation.assets[outputName].source());
+        delete compilation.assets[outputName];
+        childCompilation.assets = {};
+        resolve(stats);
       }
     });
   });