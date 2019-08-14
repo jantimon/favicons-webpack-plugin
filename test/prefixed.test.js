brunocodutra/webapp-webpack-plugin

fix: do not interpolate empty prefix
closes #50

test/prefixed.test.js
@@ -1,0 +1,23 @@
+const test = require('ava');
+const path = require('path');
+const fs = require('fs-extra');
+const WebappWebpackPlugin = require('../src');
+
+const {logo, generate, mkdir, compare, expected} = require('./util');
+
+test.beforeEach(async t => t.context.root = await mkdir());
+
+test('should allow configuring the output prefix', async t => {
+  const dist = path.join(t.context.root, 'dist');
+  const stats=  await generate({
+    context: t.context.root,
+    output: {
+      path: dist,
+    },
+    plugins: [new WebappWebpackPlugin({logo, prefix: 'path/name-'})],
+  });
+
+  t.deepEqual(await compare(dist, path.resolve(expected, 'prefixed')), []);
+});
+
+test.afterEach(t => fs.remove(t.context.root));

jantimon/favicons-webpack-plugin

test/prefixed.test.js
@@ -1,0 +1,23 @@
+const test = require('ava');
+const path = require('path');
+const fs = require('fs-extra');
+const FaviconsWebpackPlugin = require('../src');
+
+const {logo, generate, mkdir, compare, expected} = require('./util');
+
+test.beforeEach(async t => t.context.root = await mkdir());
+
+test('should allow configuring the output prefix', async t => {
+  const dist = path.join(t.context.root, 'dist');
+  const stats=  await generate({
+    context: t.context.root,
+    output: {
+      path: dist,
+    },
+    plugins: [new FaviconsWebpackPlugin({logo, prefix: 'path/name-'})],
+  });
+
+  t.deepEqual(await compare(dist, path.resolve(expected, 'prefixed')), []);
+});
+
+test.afterEach(t => fs.remove(t.context.root));