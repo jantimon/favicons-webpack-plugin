a300b9db9ce62b40bcb70020fe4d98d77a439469
Tofandel <adrien.foulon@tukan.hu>
2019-11-04 22:28:54 +0100

    Fix copy paste variable name

--- src/index.js
+++ src/index.js
@@ -105,7 +105,7 @@ module.exports = class FaviconsWebpackPlugin {
   generateFaviconsLight(compiler, compilation) {
     return new Promise((resolve, reject) => {
       const logoFileName = path.resolve(compilation.compiler.context, this.options.logo);
-      const publicPath = typeof publicPathOption !== 'undefined' ? publicPathOption : ( typeof compilation.outputOptions.publicPath !== 'undefined' ? compilation.outputOptions.publicPath : '/');
+      const publicPath = typeof this.options.publicPath !== 'undefined' ? this.options.publicPath : ( typeof compilation.outputOptions.publicPath !== 'undefined' ? compilation.outputOptions.publicPath : '/');
       const faviconExt = path.extname(this.options.logo);
       // Copy file to output directory
       compiler.inputFileSystem.readFile(logoFileName, (err, content) => {
