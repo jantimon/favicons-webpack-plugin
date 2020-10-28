d15d92fe7f39ea78d2d27306aac1bf2808faad4d
Jan Nicklas <j.nicklas@me.com>
2020-10-28 14:57:58 +0100

    feat: use the processAssets webpack api to add assets

--- src/compiler.d.ts
+++
@@ -1,6 +0,0 @@
-export function run(faviconOptions: any, context: any, compilation: any): Promise<any>;
-/**
- * faviconsPublicPath always wins over compilerPublicPath
- * If both are undefined fallback to '/'
- */
-export function getPublicPath(faviconsPublicPath: any, compilerPublicPath: any): any;
