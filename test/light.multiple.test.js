03c351434600c5f8eded4757f3efa9f728cd0de1
Jan Nicklas <j.nicklas@me.com>
2021-01-29 09:37:10 +0100

    fix: allow compiling multiple icon sets

---
+++ test/light.multiple.test.js
@@ -0,0 +1,27 @@
+const test = require('ava');
+const path = require('path');
+const fs = require('fs-extra');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('../');
+
+const { logo, mkdir, generate, snapshotCompilationAssets } = require('./util');
+
+test.beforeEach(async t => (t.context.root = await mkdir()));
+
+test('should generate multiple light icons', async t => {
+  const dist = path.join(t.context.root, 'dist');
+  const compilationStats = await generate({
+    context: t.context.root,
+    output: {
+      path: dist,
+      publicPath: '/'
+    },
+    plugins: [
+      new HtmlWebpackPlugin(),
+      new FaviconsWebpackPlugin({ logo, mode: 'light' }),
+      new FaviconsWebpackPlugin({ logo, mode: 'light', prefix: 'second/' })
+    ]
+  });
+
+  snapshotCompilationAssets(t, compilationStats);
+});
